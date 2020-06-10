4ccbff866f0ba0664f9cea3c56be6afe16aaa310
Ubuntu <ubuntu@ip-172-31-53-225.ec2.internal>
2020-06-10 21:29:15 +0000

    update env for pg_restore

--- wipe.sh
+++ wipe.sh
@@ -10,4 +10,4 @@ sudo docker exec -it $REDIS_CONTAINER redis-cli FLUSHDB;
 sudo docker exec -it $CHESTSTORE_CONTAINER npm run migrate;
 
 sudo docker cp ./cheststore.backup $POSTGRES_CONTAINER:/tmp/cheststore.backup
-sudo docker exec -it $POSTGRES_CONTAINER PGPASSWORD=cheststore pg_restore -U postgres -d cheststore -a /tmp/cheststore.backup
+sudo docker exec -e PGPASSWORD="cheststore" -it $POSTGRES_CONTAINER pg_restore -U postgres -d cheststore -a /tmp/cheststore.backup
