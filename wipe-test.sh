45369f14bedfa8ca1f1f0120d04ae3229a3eabe9
Lance Whatley <whatl3y@gmail.com>
2020-06-10 13:32:39 -0400

    fix echo

--- wipe-test.sh
+++ wipe-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 echo "PostgreSQL container $POSTGRES_CONTAINER";
-exho "redis container $REDIS_CONTAINER";
+echo "redis container $REDIS_CONTAINER";
 echo "chest.store container $CHESTSTORE_CONTAINER";
 
 sudo docker exec -it $POSTGRES_CONTAINER psql -d postgres -U postgres -c "DROP DATABASE cheststore;";
