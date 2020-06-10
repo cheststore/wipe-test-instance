8b0b49741b2e0a7195d1e1e1fdcaf6db00b4f4eb
Lance Whatley <whatl3y@gmail.com>
2020-06-10 11:12:39 -0400

    Add redis flush

--- wipe-test.sh
+++ wipe-test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 
 echo "PostgreSQL container $POSTGRES_CONTAINER";
+exho "redis container $REDIS_CONTAINER";
 echo "chest.store container $CHESTSTORE_CONTAINER";
 
 sudo docker exec -it $POSTGRES_CONTAINER psql -d postgres -U postgres -c "DROP DATABASE cheststore;";
 sudo docker exec -it $POSTGRES_CONTAINER psql -d postgres -U postgres -c "CREATE DATABASE cheststore;";
+sudo docker exec -it $REDIS_CONTAINER redis-cli FLUSHDB;
 sudo docker exec -it $CHESTSTORE_CONTAINER npm run migrate;
