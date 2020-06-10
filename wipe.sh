808d27045969b1e9e6386e8fe62511d607969aac
Lance Whatley <whatl3y@gmail.com>
2020-06-10 17:24:44 -0400

    Update sh name, add pg_dump backup file for restore on reset

--- wipe.sh
+++ wipe.sh
@@ -8,3 +8,6 @@ sudo docker exec -it $POSTGRES_CONTAINER psql -d postgres -U postgres -c "DROP D
 sudo docker exec -it $POSTGRES_CONTAINER psql -d postgres -U postgres -c "CREATE DATABASE cheststore;";
 sudo docker exec -it $REDIS_CONTAINER redis-cli FLUSHDB;
 sudo docker exec -it $CHESTSTORE_CONTAINER npm run migrate;
+
+sudo docker cp ./cheststore.backup $POSTGRES_CONTAINER:/tmp/cheststore.backup
+sudo docker exec -it $POSTGRES_CONTAINER PGPASSWORD=cheststore pg_restore -U postgres -d cheststore -a /tmp/cheststore.backup
